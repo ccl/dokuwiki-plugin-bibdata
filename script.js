75bad543bd239003a9215688c290ed8d48167886
Christoph Clausen <christoph.clausen@gmail.com>
2013-05-17 13:53:39 +0200

    Re-implementation of javascript based on jQuery.

--- script.js
+++ script.js
@@ -1,117 +1,83 @@
-var myAjax = new sack();
-var warn_color = 'darkorange';
+var bibdata_warn_color = 'darkorange';
 
-function checkSource() {
-    var source_input = $('bibdataform__source');
-    var status_div = $('bibdataform__status');
-    var bibsource = source_input.value.toLowerCase();
-    var doi_pos = bibsource.indexOf('doi');
-    var abstract_pos = bibsource.indexOf('abstract');
-    status_div.innerHTML = 'BibTeX source seems ok!';
-    status_div.style.color = 'green';
-    if(doi_pos == -1) {
-        status_div.style.color = warn_color;
-        status_div.innerHTML = 'BibTeX source does not seem to contain the doi!';
-    }
-    if(abstract_pos == -1) {
-        if(status_div.style.color == warn_color) {
-            status_div.innerHTML = status_div.innerHTML.substr(0, status_div.innerHTML.length-1)
-                + ', nor the abstract!';
-        } else {
-            status_div.style.color = warn_color;
-            status_div.innerHTML = 'BibTeX source does not seem to contain the abstract!';
-        }
-    }
-}
-
-function checkSourceCompleted() {
-    var status_div = $('bibdataform__status');
-    var source_input = $('bibdataform__source');
-    var pageid_input = $('bibdataform__pageid');
-    status_div.style.color = 'green';
-    status_div.innerHTML = 'BibTeX seems ok!';
-    eval('var result = ' + myAjax.response);
+function checkSourceCompleted(response) {
+    var status_div = jQuery('#bibdataform__status');
+    var source_input = jQuery('#bibdataform__source');
+    var pageid_input = jQuery('#bibdataform__pageid');
+    status_div.css('color', 'green');
+    status_div.html('BibTeX seems ok!');
+    eval('var result = ' + response);
     if(result.msg.length > 0) {
-        status_div.style.color = warn_color;
-        status_div.innerHTML = result.msg;
+        status_div.css('color', bibdata_warn_color);
+        status_div.html(result.msg);
         return;
     }
-    pageid_input.value = result.pageid;
+    pageid_input.val(result.pageid);
     if(!result.has_doi) {
-        status_div.style.color = warn_color;
-        status_div.innerHTML = 'BibTeX source does not seem to contain the doi!';
+        status_div.css('color', bibdata_warn_color);
+        status_div.html('BibTeX source does not seem to contain the doi!');
     }
     if(!result.has_abstract) {
-        if(status_div.style.color == warn_color) {
-            status_div.innerHTML = status_div.innerHTML.substr(0, status_div.innerHTML.length-1)
-                + ', nor the abstract!';
+        if(!result.has_doi) {
+            status_div.html(status_div.html().substr(0, status_div.html().length-1)
+			    + ', nor the abstract!');
         } else {
-            status_div.style.color = warn_color;
-            status_div.innerHTML = 'BibTeX source does not seem to contain the abstract!';
+            status_div.css('color', bibdata_warn_color);
+            status_div.html('BibTeX source does not seem to contain the abstract!');
         }
     }
 }
 
-function checkPageIdCompleted() {
-    var pageid_input = $('bibdataform__pageid');
-    if(myAjax.response == "true") {
-        pageid_input.style.color = 'green';
+function checkPageIdCompleted(response) {
+    var pageid_input = jQuery('#bibdataform__pageid');
+    if(response == "true") {
+        pageid_input.css('color', 'green');
     } else {
-        pageid_input.style.color = 'red';
+        pageid_input.css('color', 'red');
     }
 }
 
 function checkSourceAjax() {
-    var status_div = $('bibdataform__status');
-    var source_input = $('bibdataform__source');
-    var targetns = document.getElementsByName('targetns')[0];
+    var status_div = jQuery('#bibdataform__status');
+    var source_input = jQuery('#bibdataform__source');
+    var targetns = jQuery('[name="targetns"]');
 
-    status_div.innerHTML = 'Please wait while parsing BibTeX...';
-    myAjax.requestFile = DOKU_BASE + 'lib/plugins/bibdata/ajax/checkBibtex.php';
-    myAjax.method = "POST";
-    myAjax.setVar('source', encodeURIComponent(source_input.value));
-    myAjax.setVar('targetns', encodeURIComponent(targetns.value));
-    myAjax.onCompletion = checkSourceCompleted;
-    myAjax.runAJAX();
+    status_div.html('Please wait while parsing BibTeX...');
+    jQuery.post(DOKU_BASE + 'lib/plugins/bibdata/ajax/checkBibtex.php',
+		{source: encodeURIComponent(source_input.val()),
+		 targetns: encodeURIComponent(targetns.val())},
+		checkSourceCompleted);
 }
 
 function checkPageIdAjax() {
-    var pageid_input = $('bibdataform__pageid');
-    var targetns = document.getElementsByName('targetns')[0];
+    var pageid_input = jQuery('#bibdataform__pageid');
+    var targetns = jQuery('[name="targetns"]');
 
-    if(myAjax.URLString.length > 0) {
-        // Ajax not ready yet
-        setTimeout("checkPageIdAjax()", 100);
-        return;
-    }
-    myAjax.requestFile = DOKU_BASE + 'lib/plugins/bibdata/ajax/checkPageId.php';
-    myAjax.method = "POST";
-    myAjax.setVar('pageid', encodeURIComponent(pageid_input.value));
-    myAjax.setVar('targetns', encodeURIComponent(targetns.value));
-    console.log(myAjax.URLString);
-    myAjax.onCompletion = checkPageIdCompleted;
-    myAjax.runAJAX();
+    jQuery.post(DOKU_BASE + 'lib/plugins/bibdata/ajax/checkPageId.php',
+		{pageid: encodeURIComponent(pageid_input.val()),
+		 targetns: encodeURIComponent(targetns.val())},
+		checkPageIdCompleted);
 }
 
 function installFormCheck()
 {
-    var source_input = $('bibdataform__source');
-    var pageid_input = $('bibdataform__pageid');
-    if(source_input !== null) {
-        addEvent(source_input, 'change', checkSourceAjax);
+    var source_input = jQuery('#bibdataform__source');
+    var pageid_input = jQuery('#bibdataform__pageid');
+    if(source_input.length > 0) {
+        jQuery(source_input).change(checkSourceAjax);
     }
-    if(pageid_input !== null) {
-        addEvent(pageid_input, 'keyup', checkPageIdAjax);
+    if(pageid_input.length > 0) {
+        jQuery(pageid_input).keyup(checkPageIdAjax);
     }
 }
 
 function redirectOnSuccess() {
-    var success_div = $('bibdataform__success');
-    if(success_div !== null) {
-        var dest = success_div.children[0].href;
+    var success_div = jQuery('#bibdataform__success');
+    if(success_div.length > 0) {
+        var dest = success_div.attr('href');
         setTimeout(function() {window.location =  dest;}, 3000);
     }
 }
 
-addInitEvent(installFormCheck);
-addInitEvent(redirectOnSuccess);
+jQuery(installFormCheck);
+jQuery(redirectOnSuccess);
